0661df78fdf5befe974a82beed2c371794985e16
Polishek <bartusdera@gmail.com>
2021-12-10 02:26:47 +0100

    Utils.kt with new method called readInputAsInts.

--- src/Utils.kt
+++ src/Utils.kt
@@ -8,6 +8,16 @@ import java.security.MessageDigest
 fun readInput(name: String) = File("src", "$name.txt").readLines()
 
 /**
+ * Reads lines from the given input txt file as integers.
+ */
+fun readInputAsInts(name: String) = File("src", "$name.txt").readLines().map { it.toInt() }
+
+/**
+ * Reads lines from the given input txt file as integers.
+ */
+fun readInputAsCharArray(name: String) = File("src", "$name.txt").readLines().map { it.toCharArray() }
+
+/**
  * Converts string to md5 hash.
  */
 fun String.md5(): String = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray())).toString(16)
