33b9ba4e8900f615db95c9ee5544fd99d1ee36b6
Polishek <bartusdera@gmail.com>
2021-12-10 02:25:25 +0100

    Day 3, part 1

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,13 +1,38 @@
+import kotlin.math.pow
+
 fun main() {
-    fun part1(input: List<Int>): Int {
-        return input.windowed(size = 2).count { (a, b) -> a < b }
+    fun part1(input: List<String>): Int {
+
+        var gammaRate = 0
+        var epsilonRate = 0
+        var correctBit : Int
+        val binaryLength = input.get(1).length
+
+        for(i in 0 until binaryLength){
+            correctBit = 0
+
+            for(binary in input){
+                if(binary[i] == '1'){
+                    correctBit++
+                }
+            }
+
+            if ( (input.size - correctBit) < correctBit ){
+                gammaRate += 2.0.pow(binaryLength - 1 - i).toInt()
+            }
+            else{
+                epsilonRate += 2.0.pow(binaryLength - 1 - i).toInt()
+            }
+        }
+
+        return gammaRate * epsilonRate
     }
 
-    fun part2(input: List<Int>): Int {
-        return input.windowed(size = 3).windowed(size = 2).count { (a, b) ->  a.sum() < b.sum() }
+    fun part2(input: List<String>): Int {
+        return 0
     }
 
-    val input = readInputAsInts("Day01")
+    val input = readInput("Day03")
     println(part1(input))
     println(part2(input))
 }
